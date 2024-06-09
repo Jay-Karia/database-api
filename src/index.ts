19c466e6e919d2e5ba399f7b37db1ebb64464dba
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-06-09 22:03:33 +0530

    add bearer authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Hono } from 'hono'
 import connectDB from "./config/db"
+import { bearerAuth } from 'hono/bearer-auth'
 
 const app = new Hono()
 
+const token = Bun.env.BEARER_TOKEN || ""
+
+app.use('/api/*', bearerAuth({ token }))
+
 app.get('/', (c) => {
   connectDB()
-  return c.text('Hello Hono!')
+  return c.text("Hello world")
+})
+
+app.get('/api/page', (c) => {
+  return c.json({ message: 'You are authorized' })
 })
 
 export default app
